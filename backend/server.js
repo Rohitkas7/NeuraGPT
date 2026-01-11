fd05bf71170991ed15a2a19df4ac17037ae6c413
Rohit Kumar <rohit9801742342@gmail.com>
2026-01-11 21:41:17 +0530

    Update server.js to use dynamic PORT for Render

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 const app = express();
-const PORT = 8080;
+
+const PORT = process.env.PORT || 8080;
+
+/* const PORT = 8080; */
 
 app.use(express.json());
 app.use(cors());
